0ca26677e0ba9ba33a108f8d9a8ff444728ba061
Gustavo Barbosa <gustavo.barbosabd@gmail.com>
2021-12-02 18:43:45 -0300

    Criação de componentes Produtos e metodos get post, update, delete

---
+++ front/src/app/shared/components/produto-component/produto-list/produto-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-list',
+  templateUrl: './produto-list.component.html',
+  styleUrls: ['./produto-list.component.scss']
+})
+export class ProdutoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
